4535c5d4937f6ca6015eb9565f7594d9319f7284
Kelsey Campbell <campkels@gmail.com>
2020-02-16 22:56:41 -0500

    Lyric merge process feat. Genius API

--- process/s3_merge_lyrics.py
+++ process/s3_merge_lyrics.py
@@ -1,20 +1,47 @@
+################################################################################
+# Step 3: Merge Lyrics
+#
+# Script merges lyrics for Billboard songs listed in song-data.csv by either
+# pulling from local collection of .txt lyric files or using the Genius API.
+#
+################################################################################
+
 # Import packages
 #---------------------------------------------
 import pandas as pd
 import os
 import string
+import re 
+import lyricsgenius # pip install lyricsgenius
+import argparse
 
-# Read in data
+# Setup
 #---------------------------------------------
 
 # Paths
 csvpath = './output/song-data.csv'
 lyricspath = './billboard_lyrics/lyrics/lyrics'
+manualpath = './output/genius-manual.csv'
+outputpath = './output/song-data-lyrics.csv'
 
 # Read in songs data and list of lyrics files
 data = pd.read_csv(csvpath)
 lyricsfiles = [s for s in os.listdir(lyricspath)]
 
+# Setup lyricsgenius access - Command line will need user's Genius "Access Token" (see documentation)
+parser = argparse.ArgumentParser(description = 'Merge lyrics with Billboard songs')
+parser.add_argument('geniustoken', nargs=1, type=str)
+args = parser.parse_args()
+accesstoken = args.geniustoken[0]
+genius = lyricsgenius.Genius(accesstoken)
+
+# Read in manual genius input changes and create lookup table
+manual = pd.read_csv(manualpath)
+manual['songartist'] = manual['song'] + "-" + manual['artist']
+geniuslookup = manual[['songartist', 'geniussong', 'geniusartist']].set_index('songartist').to_dict('index')
+
+# Add additional characters to remove from lyric text
+string.punctuation = string.punctuation + "’…—”“,¡¿"
 
 # Functions
 #---------------------------------------------
@@ -32,33 +59,69 @@ def getLyricFilename(row):
 	lyricsfile = artist_search + '-' + song_search + ".txt"
 	return lyricsfile
 
+def getGeniusParameters(row):
+	'''
+	Takes a row of songs dataframe
+	Checks if song is in the lookup table for manually verified Genius API parameters.
+	(Future applications might want to replace *'s in song titles and take 1st artist of multiple artist entries to reduce manual lookups)
+	If not in manual lookup table, the song and artist from the original data is sufficient (manually verified)
+	'''
+	# Create row key for lookup table
+	songartist = row['song'] + "-" + row['artist']
+	# Check if in manual lookup dictionary, get verified parameters if so
+	if songartist in geniuslookup.keys():
+		song = geniuslookup[songartist]['geniussong']
+		artist = geniuslookup[songartist]['geniusartist']
+	# If not in lookup table, original song and artist entries are sufficient parameters for Genius API (manually verified during testing)
+	else:
+		song = row['song']
+		artist = row['artist']
+	return song, artist
+
 def cleanLyric(text):
 	'''
 	Takes string
-	Cleans lyric text (removes punctuation, lowercase)
+	Cleans lyric text (removes line breaks, punctuation, lowercase, etc)
 	'''
+	# Remove Genius annotations, if in [] only - one song puts in () and does not get stripped (won't affect analysis)
+	text = re.sub("[\[].*[\]]", " ", text)
+	# Remove line breaks
+	text = text.replace("\n", " ")
 	# Remove punctuation
 	text = text.translate(str.maketrans(dict.fromkeys(string.punctuation)))
 	# Remove capitals
 	text = text.lower()
+	# Remove multiple spaces
+	text = re.sub(" +", " ", text).strip()
+	# Remove weird empty string thing
+	text = text.replace("â€‹", "")
 	return text
 
 def getLyrics(row):
 	'''
 	Takes row of songs dataframe
-	Pulls lyrics for each song in dataframe. Most songs are included in stored Billboard lyric files. Lyrics are scraped (TODO) for those not included.
+	Pulls lyrics for each song in dataframe. Most songs are included in stored Billboard lyric files. 
+	Lyrics are pulled using the Genius API (requires credentials) for those not included.
 	'''
 	# Get lyrics filename (for saved lyrics)
 	lyricsfile = getLyricFilename(row)
 	# Pull from saved billboard files if exists
 	if lyricsfile in lyricsfiles:
 		with open('./billboard_lyrics/lyrics/lyrics/{}'.format(lyricsfile), 'r') as file:
-		    lyric = file.read().replace('\n', '')
+		    lyric = file.read()
 		    lyric = cleanLyric(lyric)
-	# Scrape lyrics if not in saved files
+	# Get lyrics using lyricsgenius API if not in saved files
 	else:
-		lyric = "not in saved files"
-	return(lyric)
+		# Get parameters
+		song, artist = getGeniusParameters(row)
+		# Query Genius API
+		geniussong = genius.search_song(song, artist)
+		if geniussong != None:
+			lyric = geniussong.lyrics
+			lyric = cleanLyric(lyric)
+		else:
+			lyric = "ADD MANUALLY"
+	return lyric
 
 # Data process
 #---------------------------------------------
@@ -69,4 +132,60 @@ data.drop_duplicates(subset = ['song', 'artist', 'gender'], keep = 'first', inpl
 # Get Lyrics
 data['lyrics'] = data.apply(getLyrics, axis=1)
 
-print(data.head(20))
+# Write out
+data.to_csv(outputpath, index=False, encoding='utf-8-sig')
+
+
+
+# Test missing songs only to see if they get pulled correctly by lyrics genius
+
+#print(data.head(20))
+# print(data[data['lyrics'] == "not in saved files"].shape)
+
+
+# missing = data[data['lyrics'] == "not in saved files"]
+# missing = missing.reset_index(drop=True).reset_index()
+# #missing.to_csv('./output/missing-lyrics.csv', index=False)
+# print(missing.head())
+# print(missing.shape)
+
+# def getMissLyrics(row):
+# 	'''
+# 	'''
+# 	print(row['index'])
+# 	song = genius.search_song(row['song'], row['artist'])
+# 	if song != None:
+# 		lyric = song.lyrics
+# 		lyric = cleanLyric(lyric)
+# 	else:
+# 		lyric = "ADD MANUALLY"
+# 	print(lyric)
+# 	return(lyric)
+
+# missing['lyrics_test'] = missing.apply(getMissLyrics, axis=1)
+# missing.to_csv('./output/missing-lyrics-test.csv', index=False)
+
+
+# artist = genius.search_artist("Jibbs")
+# print(artist.songs)
+
+# song = genius.search_song("You Need To Calm Down", "Taylor Swift")
+# song = genius.search_song("Walk Me Home", "P!nk")
+# song = genius.search_song("Chain Hang Low", "Jibbs")
+# song = genius.search_song("Poop", "Jibbs")
+# print(song.lyrics)
+
+# teststr = "ddy ¡ay daddy play n skillz katy perry ¡vamos ¿cómo te llamas be"
+
+# print(cleanLyric(teststr))
+# print(teststr.replace("’", ""))
+# print(string.punctuation + "’")
+
+# song = genius.search_song("I'm A Flirt", "R. Kelly")
+# print(song.lyrics)
+# print(cleanLyric(song.lyrics))
+
+# with open('./billboard_lyrics/lyrics/lyrics/Nick_Jonas-Jealous.txt', 'r') as file:
+# 		    lyric = file.read() #.replace('\n', '')
+# 		    #lyric = cleanLyric(lyric)
+# 		    print(lyric)
